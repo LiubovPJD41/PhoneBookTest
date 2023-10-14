867913dac56d76d3d7fd0b350170abcf9da3a655
Liubov P. JD-41 <lovely8419@mail.ru>
2023-10-14 07:46:13 +0100

    Tests

---
+++ src/test/java/Polyaeva/PhoneBookTest.java
@@ -0,0 +1,40 @@
+package Polyaeva;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.annotation.Testable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Testable
+public class PhoneBookTest {
+    private PhoneBook phoneBook;
+
+    @BeforeEach
+    public void setup() {
+        phoneBook = new PhoneBook();
+    }
+
+    @Test
+    public void addGroupTest() {
+        String groupName = "Friends";
+        assertTrue(phoneBook.addGroup(groupName));
+    }
+
+    @AfterEach
+    public void tearDown(){
+        phoneBook = null;
+    }
+    @Test
+    public void findContactInGroupTest(){
+        String groupName = "Friends";
+        String name = "Liubov";
+
+        Contact contactExpected = null;
+        Contact contact = phoneBook.findContactInGroup(name,groupName);
+
+        assertEquals(contactExpected, contact);
+    }
+}
